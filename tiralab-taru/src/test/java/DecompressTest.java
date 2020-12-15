21da2eb5d55ad20f09eed95632b1bb559da6a1c7
Taru Saloheimo <taru.saloheimo@gmail.com>
2020-12-15 20:19:19 +0200

    delete file

--- tiralab-taru/src/test/java/DecompressTest.java
+++ tiralab-taru/src/test/java/DecompressTest.java
@@ -5,6 +5,9 @@ import app.tiralab.taru.InputText;
 import app.tiralab.taru.MyMap;
 import app.tiralab.taru.Output;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.junit.After;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -13,13 +16,18 @@ import org.junit.Test;
 public class DecompressTest {
 
 
+    @After
+    public void cleanup() throws IOException {
+        Files.deleteIfExists(Paths.get("dummy2.dc"));
+    }
+    
     @Test 
     public void processTest() throws IOException {
         InputText input = new InputText("abbacabba55");
         HuffmanTree tree = new HuffmanTree(input);
         tree.create();
         tree.getPrefixes();       
-        Output output = new Output(tree, input, "dummy");
+        Output output = new Output(tree, input, "dummy1");
         String s = output.createPrefixString();
         
         Decompress dc = new Decompress(s, tree, "dummy2.huff");
